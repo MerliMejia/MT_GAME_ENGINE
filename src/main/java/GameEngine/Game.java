ac3853638db22d8d485e4aeb29e5edec2be2218d
Merli Mejia <merlimejia25@gmail.com>
2021-09-19 18:50:17 -0400

    Drew a square!
    A more organized approach to load raw models and render them.

--- src/main/java/GameEngine/Game.java
+++ src/main/java/GameEngine/Game.java
@@ -1,6 +1,5 @@
 package GameEngine;
 
-import org.lwjgl.BufferUtils;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.glfw.GLFWVidMode;
 import org.lwjgl.opengl.GL;
@@ -11,18 +10,6 @@ import java.nio.IntBuffer;
 import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.glfw.GLFW.glfwPollEvents;
-import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
-import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
-import static org.lwjgl.opengl.GL11.GL_FLOAT;
-import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
-import static org.lwjgl.opengl.GL11.glClear;
-import static org.lwjgl.opengl.GL11.glDrawArrays;
-import static org.lwjgl.opengl.GL15.*;
-import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
-import static org.lwjgl.opengl.GL20.*;
-import static org.lwjgl.opengl.GL20.glDisableVertexAttribArray;
-import static org.lwjgl.opengl.GL30.glBindVertexArray;
-import static org.lwjgl.opengl.GL30.glGenVertexArrays;
 import static org.lwjgl.system.MemoryStack.stackPush;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
@@ -123,109 +110,33 @@ public class Game {
         // bindings available for use.
         GL.createCapabilities();
 
-        //Creates VAO and set it to the current one
-        int vaoID = glGenVertexArrays();
-        glBindVertexArray(vaoID);
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
 
-        //Give triangle to OGL
         float[] vertices = {
-                -1.0f, -1.0f, 0.0f,
-                1.0f, -1.0f, 0.0f,
-                0.0f, 1.0f, 0.0f,
+          -0.5f, 0.5f, 0f,
+          -0.5f, -0.5f, 0f,
+          0.5f, -0.5f, 0f,
+
+          0.5f, -0.5f, 0f,
+          0.5f, 0.5f, 0f,
+          -0.5f, 0.5f, 0f
         };
-        int vertexBufferId = glGenBuffers();
-        glBindBuffer(GL_ARRAY_BUFFER, vertexBufferId);
-        glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW);
 
-        //Shaders!
-        int vertexShaderId = glCreateShader(GL_VERTEX_SHADER);
-
-        String vertexShaderData = """
-                #version 330 core
-                layout(location = 0) in vec3 vertexPosition_modelspace;
-                void main(){
-                  gl_Position.xyz = vertexPosition_modelspace;
-                  gl_Position.w = 1.0;
-                }
-                """;
-
-        glShaderSource(vertexShaderId, vertexShaderData);
-        glCompileShader(vertexShaderId);
-
-        IntBuffer vertexShaderStatusBuffer = BufferUtils.createIntBuffer(1);
-        glGetShaderiv(vertexShaderId, GL_COMPILE_STATUS, vertexShaderStatusBuffer);
-        glGetShaderiv(vertexShaderId, GL_INFO_LOG_LENGTH, vertexShaderStatusBuffer);
-
-        int status = vertexShaderStatusBuffer.get();
-
-        if (status > 0) {
-            String err = glGetShaderInfoLog(vertexShaderId, status);
-            System.out.println(err);
-        }
-
-
-        int fragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
-
-        String fragmentShaderData = """
-                #version 330 core
-                out vec3 color;
-                void main(){
-                  color = vec3(0.238,0.238,1);
-                }
-                """;
-
-        glShaderSource(fragmentShaderId, fragmentShaderData);
-        glCompileShader(fragmentShaderId);
-
-        IntBuffer fragmentShaderStatusBuffer = BufferUtils.createIntBuffer(1);
-        glGetShaderiv(fragmentShaderId, GL_COMPILE_STATUS, fragmentShaderStatusBuffer);
-        glGetShaderiv(fragmentShaderId, GL_INFO_LOG_LENGTH, fragmentShaderStatusBuffer);
-
-        int fragmentstatus = fragmentShaderStatusBuffer.get();
-
-        if (fragmentstatus > 0) {
-            String err = glGetShaderInfoLog(fragmentShaderId, fragmentstatus);
-            System.out.println(err);
-        }
-
-        int programID = glCreateProgram();
-        glAttachShader(programID, vertexShaderId);
-        glAttachShader(programID, fragmentShaderId);
-        glLinkProgram(programID);
-
-        IntBuffer programResultBuffer = BufferUtils.createIntBuffer(1);
-        glGetProgramiv(programID, GL_LINK_STATUS, programResultBuffer);
-        glGetProgramiv(programID, GL_INFO_LOG_LENGTH, programResultBuffer);
-        int programStatus = programResultBuffer.get();
-
-        if (programStatus > 0) {
-            System.out.println(glGetProgramInfoLog(programID, programStatus));
-        }
-
-        glDetachShader(programID, vertexShaderId);
-        glDetachShader(programID, fragmentShaderId);
-        glDeleteShader(vertexShaderId);
-        glDeleteShader(fragmentShaderId);
-
+        RawModel model = loader.loadToVAO(vertices);
 
         // Run the rendering loop until the user has attempted to close
         // the window or has pressed the ESCAPE key.
         while (!glfwWindowShouldClose(window)) {
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
-
-            //Use our shaders
-            glUseProgram(programID);
-            //Draw our triangle from VAO
-            glEnableVertexAttribArray(0);
-            glBindBuffer(GL_ARRAY_BUFFER, vertexBufferId);
-            glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
-            glDrawArrays(GL_TRIANGLES, 0, 3);
-            glDisableVertexAttribArray(0);
+            renderer.prepare();
+            renderer.render(model);
             glfwSwapBuffers(window); // swap the color buffers
 
             // Poll for window events. The key callback above will only be
             // invoked during this call.
             glfwPollEvents();
         }
+
+        loader.cleanUp();
     }
 }
